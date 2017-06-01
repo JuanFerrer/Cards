4de7090d1aaa51a926c53b6e43e2be45c2fecfef
JuanFerrer <juanemilioferrer@outlook.com>
2017-06-01 10:15:09 +0100

    Creating WinAPI window

--- DesktopTest/Test.cpp
+++ DesktopTest/Test.cpp
@@ -1,17 +1,48 @@
-https://msdn.microsoft.com/en-us/library/bb384843.aspx
+// https://msdn.microsoft.com/en-us/library/bb384843.aspx
 
 #include <windows.h>  
-#include <tchar.h>  
+#include <tchar.h>
 
-int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_ HINSTANCE hPrevInstance,
-	_In_ LPSTR     lpCmdLine,
-	_In_ int       nCmdShow
-	)
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR greeting[] = _T("Hello, World!");
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		// Here your application is laid out.  
+		// For this introduction, we just print out "Hello, World!"  
+		// in the top left corner.  
+		TextOut(hdc,
+			5, 5,
+			greeting, _tcslen(greeting));
+		// End application-specific layout section.  
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+
+	static TCHAR szWindowClass[] = _T("win32app");
+	static TCHAR szTitle[] = _T("Game name");
+
 	WNDCLASSEX wcex;
-	LPCWSTR szWindowClass = L"";
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
@@ -36,8 +67,7 @@ int CALLBACK WinMain(
 		return 1;
 	}
 
-	static TCHAR szWindowClass[] = _T("win32app");
-	static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
+	auto hInst = hInstance; // Store instance handle in our global variable 
 
 	// The parameters to CreateWindow explained:  
 	// szWindowClass: the name of the application  
@@ -60,27 +90,25 @@ int CALLBACK WinMain(
 		hInstance,
 		NULL
 		);
+
 	if (!hWnd)
 	{
-		MessageBox(NULL,
-			_T("Call to CreateWindow failed!"),
-			_T("Win32 Guided Tour"),
-			NULL);
-
+		MessageBox(NULL, _T("Call to CreateWindow failed!"), _T("Win32 Guided Tour"), NULL);
 		return 1;
 	}
 
 	// The parameters to ShowWindow explained:  
 	// hWnd: the value returned from CreateWindow  
 	// nCmdShow: the fourth parameter from WinMain  
-	ShowWindow(hWnd,
-		nCmdShow);
+	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
-}
 
-LRESULT CALLBACK WndProc(
-	_In_ HWND   hwnd,
-	_In_ UINT   uMsg,
-	_In_ WPARAM wParam,
-	_In_ LPARAM lParam
-	);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
