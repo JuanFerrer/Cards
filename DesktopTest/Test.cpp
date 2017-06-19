d709a1d9319acf8632fe73029518fce4c1336a94
Juan Ferrer <juanemilioferrer@outlook.com>
2017-06-19 19:39:39 +0100

    Instance window

--- DesktopTest/Test.cpp
+++ DesktopTest/Test.cpp
@@ -1,137 +1,35 @@
 // https://www.codementor.io/malortie/win32-app-load-apply-resources-to-window-c-cpp-visual-studio-du107jdb4
 
-// Header required to help detect window version
-#include <sdkddkver.h>
-
-// Macro used to reduce namespace pollution
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#include <windows.h>  
-#include <tchar.h>
+////////////////////////////////////////////////////////////////////////////////
+// Filename: main.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "SystemClass.h"
 
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
-	TCHAR greeting[] = _T("Hello, World!");
+	SystemClass* System;
+	bool result;
 
-	switch (message)
+
+	// Create the system object.
+	System = new SystemClass;
+	if (!System)
 	{
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-
-		// Here your application is laid out.  
-		// For this introduction, we just print out "Hello, World!"  
-		// in the top left corner.  
-		TextOut(hdc,
-			5, 5,
-			greeting, _tcslen(greeting));
-		// End application-specific layout section.  
-
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-		break;
+		return 0;
 	}
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if (result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
 
 	return 0;
-}
-
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
-{
-
-	static TCHAR szWindowClass[] = _T("win32app");
-	static TCHAR szTitle[] = _T("Game name");
-
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-
-	if (!RegisterClassEx(&wcex))
-	{
-		//MessageBox(NULL,
-		//	_T("Call to RegisterClassEx failed!"),
-		//	_T("Win32 Guided Tour"),
-		//	NULL);
-
-		return 1;
-	}
-
-	auto hInst = hInstance; // Store instance handle in our global variable
-
-	// Setup window initialization attributes.
-	CREATESTRUCT cs;
-	ZeroMemory(&cs, sizeof(cs));
-
-	cs.x = 0;	// Window X position
-	cs.y = 0;	// Window Y position
-	cs.cx = 640;	// Window width
-	cs.cy = 480;	// Window height
-	cs.hInstance = hInstance; // Window instance.
-	cs.lpszClass = wcex.lpszClassName;		// Window class name
-	cs.lpszName = szWindowClass;	// Window title
-	cs.style = WS_OVERLAPPEDWINDOW;		// Window style
-
-	// The parameters to CreateWindow explained:  
-	// szWindowClass: the name of the application  
-	// szTitle: the text that appears in the title bar  
-	// WS_OVERLAPPEDWINDOW: the type of window to create  
-	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)  
-	// 500, 100: initial size (width, length)  
-	// NULL: the parent of this window  
-	// NULL: this application does not have a menu bar  
-	// hInstance: the first parameter from WinMain  
-	// NULL: not used in this application  
-	HWND hWnd = ::CreateWindowEx(
-		cs.dwExStyle,
-		cs.lpszClass,
-		cs.lpszName,
-		cs.style,
-		cs.x,
-		cs.y,
-		cs.cx,
-		cs.cy,
-		cs.hwndParent,
-		cs.hMenu,
-		cs.hInstance,
-		cs.lpCreateParams);
-
-	if (!hWnd)
-	{
-		//MessageBox(NULL, _T("Call to CreateWindow failed!"), _T("Win32 Guided Tour"), NULL);
-		return 1;
-	}
-
-	// The parameters to ShowWindow explained:  
-	// hWnd: the value returned from CreateWindow  
-	// nCmdShow: the fourth parameter from WinMain  
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return (int)msg.wParam;
 }
